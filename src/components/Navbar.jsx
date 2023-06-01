6a795f8ef84712fe117ba2c0c498cdecd78fd46a
raidMl <raidreus.22@gmail.com>
2023-06-01 21:38:12 +0100

    add 3d image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
 <Link to="/" className="flex items-center gap-2" onClick={()=>{setActive("");window.scrollTo(0,0)}}>
 <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-<p className="text-white text-[18px] font-bold cursor-pointer">Raid <span className="sm:block hidden">
+<p className="text-white text-[18px] font-bold cursor-pointer fl">Raid <span className="sm:block hidden">
   full stack developer</span></p>
 </Link>
 
@@ -29,8 +29,20 @@ const Navbar = () => {
   {/* for mobile navigation  */}
   <div className="sm:hidden flex flex-1 justify-end items center">
     <img src={toggle ? close:menu} alt="menu" className='cursor-pointer w-[28px] h-[28px] object-contain cursor-pointer' onClick={()=>setToggle(!toggle)}/>
- <div className={`${!toggle ? "hidden":'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}></div>
-
+ <div className={`${!toggle ? "hidden":'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+    <ul className="list-none flex justify-end items-start  flex-col gap-4">
+      {navLinks.map((Link)=>(
+        <li key={Link.id} className={`${active===Link.title ? "text-white":"text-secondary"}
+        font-poppins font-medium cursor-pointer text-[16px]`}
+        onClick={()=>{()=>{setActive(Link.title)}
+        setToggle(!toggle)
+      }}
+        >
+          <a href={`#${Link.id}`}>{Link.title}</a>
+        </li>
+      ))}
+    </ul>
+  </div>
   </div>
     </div>
     </nav>
