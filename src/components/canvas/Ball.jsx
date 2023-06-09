0bcf85e572b3965727d6b773aaa0cafc4f48690d
raidMl <raidreus.22@gmail.com>
2023-06-09 23:55:33 +0100

    add tech

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
-
-const Ball = () => {
+import React,{Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Decal,Float,OrbitControls,Preload,useTexture } from '@react-three/drei'
+import CanvasLoader from '../Loader'
+const Ball = (props) => {
+  const [decal]=useTexture([props.imgUrl])
   return (
     <div>Ball</div>
   )
