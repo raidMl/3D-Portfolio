290b40c3e82df90320a3aad50ddd128dce58d24f
raidMl <raidreus.22@gmail.com>
2023-06-30 12:51:50 +0100

    add 3d earth

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React, {useState,useRef,Suspense} from 'react'
+import { Canvas,useFrame } from '@react-three/fiber'
+import { Points,PointMaterial,Preload } from '@react-three/drei'
+import * as random from 'math/random/dist/'
 const Stars = () => {
   return (
     <div>Stars</div>
   )
 }
+
+const starsCanvas=()=>{return(
+<div
+
+className='w-full h-full absolute inset-0 z-[-1]'>
+  <Canvas>
+    
+  </Canvas>
+</div>
+)}
 
 export default Stars
