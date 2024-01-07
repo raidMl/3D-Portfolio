f49ff74d2d2d8312a6da7354753fa526363ebdc1
raidMl <raidreus.22@gmail.com>
2024-01-07 22:47:02 +0100

    add event to scale onclick

--- src/components/canvas/pc.jsx
+++ src/components/canvas/pc.jsx
@@ -1,14 +1,18 @@
-import { Suspense } from "react" 
+import { Suspense,useState } from "react" 
 import { Canvas } from "@react-three/fiber" //for 3d 
 import { OrbitControls,Preload,useGLTF } from "@react-three/drei"
 import CanvasLoader from '../Loader'
 
 const Pc = () => {
   const pc = useGLTF("./pc/ASUS Laptop.gltf");
+  const [active, setActive] = useState(1.5)
 
   // const earth=useGLTF('./planet/scene.gltf')
   return (
-    <mesh>
+    <mesh  scale={active ? 3 : 1} onClick={() => setActive(!active)} 
+    >
+          <meshPhongMaterial color="royalblue" />
+
          <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 150, 10]}
@@ -46,9 +50,9 @@ position:[-40,3,6]}}
         <OrbitControls
         autoRotate
         // onPointerEnter={Suspense}
-        enableZoom={true}
-        maxPolarAngle={Math.PI}
-        minPolarAngle={0}
+        enableZoom={false}
+        maxPolarAngle={Math.PI/2}
+        minPolarAngle={Math.PI/4}
         />
         <Pc/>
       </Suspense>
